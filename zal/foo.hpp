30f2d0f2279f7cf9e886d63f4017b4e2b184c982
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2020-11-30 15:53:57 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,20 +9,18 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > test;
     std::vector< char > test_rev;
-    for (Human it : people) {
-        
+    for (Human& it : people) {
+        it.birthday();
         if (it.isMonster() == true) {
             test.emplace_back('n');
-        }
+            }
         else {
             test.emplace_back('y');
-        }
+            }
         }    
 for (auto it1 = test.crbegin(); it1 != test.crend(); ++it1) {
     test_rev.emplace_back(*it1);
 }
-for (Human x : people) {
-    x.birthday();
-}
+
     return test_rev;
 }
